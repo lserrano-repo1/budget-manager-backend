6bb5013d089fd3fbb98f13f93af3b3eddb5490d6
Luis Serrano <luisernesto.serrano@gmail.com>
2023-01-20 00:56:55 -0600

    Commenting out extra logging

--- src/database/oracle.js
+++ src/database/oracle.js
@@ -40,9 +40,9 @@ module.exports.pool = async(statement, binds=[], opts={}) => {
         conn = await oracledb.getConnection();
         result = await conn.execute(statement, binds, opts);
         
-        console.log('--- RESULT TO BE RETURNED ---');
+        /*console.log('--- RESULT TO BE RETURNED ---');
         console.log(result);
-        console.log('-----------------------------');
+        console.log('-----------------------------');*/
         return result;
 
     } catch(error){
